a7f3c0846ffe162606859567980b703464b99106
Aurav <47893192+le-incroyable1-dev@users.noreply.github.com>
2021-04-20 00:00:24 +0530

    Create printElementsInRange.cpp

---
+++ binaryTree/BST/printElementsInRange.cpp
@@ -0,0 +1,46 @@
+/**********************************************************
+
+	Following is the Binary Tree Node class structure
+
+	template <typename T>
+	class BinaryTreeNode {
+    	public : 
+    	T data;
+    	BinaryTreeNode<T> *left;
+    	BinaryTreeNode<T> *right;
+
+    	BinaryTreeNode(T data) {
+        	this -> data = data;
+        	left = NULL;
+        	right = NULL;
+    	}
+	};
+
+***********************************************************/
+
+void elementsInRangeK1K2(BinaryTreeNode<int>* root, int k1, int k2) {
+	// Write your code here
+    if(root == NULL)
+        return;
+    else
+    {
+        if(root->data < k1)
+        {
+            elementsInRangeK1K2(root->right, k1, k2);
+        }
+        else if(root->data > k2)
+        {
+            elementsInRangeK1K2(root->left, k1, k2);
+        }
+        else
+        {
+            if(root->left != NULL && root->left->data >= k1)
+                elementsInRangeK1K2(root->left, k1, k2);
+            
+            cout << root->data << " ";
+            
+            if(root->right != NULL && root->right->data <= k2)
+                elementsInRangeK1K2(root->right, k1, k2);
+        }
+    }
+}
